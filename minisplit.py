5945e05447369766b810fb4c09b7264c4c81a908
Mircea Trofin <mtrofin@gmail.com>
2022-04-09 07:07:51 -0700

    Added mixed mode support for minisplit

--- minisplit.py
+++ minisplit.py
@@ -3,26 +3,47 @@
 import broadlink
 import device
 import datetime
+import enum
 import gin
 import os
 
 from pathlib import Path
 from typing import List
 
+@gin.constants_from_enum
+class Mode(enum.Enum):
+  Invalid = 0
+  Heat = 1
+  Cool = 2
+  Mixed = 3
+
 @gin.configurable
 class Minisplit(device.Device):
 
-  def __init__(self, on_cmd:bytes, off_cmd:bytes, schedule:List[int]):
+  def __init__(self, heating_cmd: bytes, cooling_cmd: bytes, mode: Mode,
+               off_cmd: bytes, schedule: List[int],
+               mixed_mode_heating_period: List[int]):
     super(Minisplit, self).__init__('minisplit')
-    self._on_cmd = on_cmd
+    assert mode != Mode.Invalid
+    self._mode = mode
+    self._heating_cmd = heating_cmd
+    self._cooling_cmd = cooling_cmd
     self._off_cmd = off_cmd
     self._device = broadlink.discover()[0]
     self._device.auth()
     self._schedule = schedule
+    self._mixed_mode_heating_period = mixed_mode_heating_period
 
+  def _get_on_cmd(self):
+    if self._mode == Mode.Heat or \
+            (self._mode == Mode.Mixed and \
+             (datetime.datetime.now().hour in self._mixed_mode_heating_period)):
+      return self._heating_cmd
+    return self._cooling_cmd
+    
   def turn_on(self):
     try:
-      self._device.send_data(self._on_cmd)
+      self._device.send_data(self._get_on_cmd())
       return True
     except:
       return False
--- minisplit_test.py
+++ minisplit_test.py
@@ -1,3 +1,4 @@
+import datetime
 import unittest
 import minisplit
 
@@ -11,6 +12,21 @@ class TestMinisplit(unittest.TestCase):
   def test_off(self):
     ms = minisplit.Minisplit()
     self.assertTrue(ms.turn_off())
+  
+  def test_modes(self):
+    ms = minisplit.Minisplit()
+    hr = datetime.datetime.now().hour
+    sched = [hr - 1, hr, hr + 1]
+    ms._mixed_mode_heating_period = sched
+    ms._mode = minisplit.Mode.Cool
+    self.assertEqual(ms._get_on_cmd(), ms._cooling_cmd)
+    ms._mode = minisplit.Mode.Heat
+    self.assertEqual(ms._get_on_cmd(), ms._heating_cmd)
+    ms._mode = minisplit.Mode.Mixed
+    self.assertEqual(ms._get_on_cmd(), ms._heating_cmd)
+    ms._mixed_mode_heating_period = [hr + 2, hr + 3 ]
+    self.assertEqual(ms._get_on_cmd(), ms._cooling_cmd)
+
 
 if __name__ == '__main__':
   unittest.main()
